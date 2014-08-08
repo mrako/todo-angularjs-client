243ecd8f6a1d6b5a40d89cf9764eec70f2c96451
Marko Klemetti <marko.klemetti@gmail.com>
2014-08-08 13:28:56 +0300

    Add save button

--- app/scripts/controllers/todos.js
+++ app/scripts/controllers/todos.js
@@ -1,5 +1,7 @@
 angular.module('todoApp').controller('TodosCtrl', function($scope, todosService) {
 
+  $scope.formData = {};
+
   todosService.list()
     .success(function(data) {
       $scope.todos = data;
