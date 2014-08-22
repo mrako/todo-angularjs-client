969a3d10ea2821a2ae50fedc00b2a063079f3055
Marko Klemetti <marko.klemetti@gmail.com>
2014-08-22 11:42:30 +0300

    Removed form to work as an exercise

--- app/scripts/controllers/todos.js
+++ app/scripts/controllers/todos.js
@@ -1,62 +1,43 @@
 'use strict';
 
 angular.module('todoApp').controller('TodosCtrl', function($scope, todosService) {
-    $scope.formData = {};
-    $scope.submit = 'Create';
+  $scope.formData = {};
 
-    // GET =====================================================================
-    todosService.list()
+  // GET =====================================================================
+  todosService.list()
+    .success(function(data) {
+      $scope.todos = data;
+    });
+
+  // CREATE AND SAVE =========================================================
+  $scope.save = function() {
+    if ($scope.formData._id === undefined) {
+      todosService.create($scope.formData)
         .success(function(data) {
-            $scope.todos = data;
+          $scope.todos = data;
         });
+    } else {
+      todosService.update($scope.formData._id, $scope.formData)
+        .success(function(data) {
+          $scope.todos = data;
+        });
+    }
+    $scope.formData = {};
+  };
 
-    // CREATE AND SAVE =========================================================
-    $scope.save = function() {
-        if ($scope.formData._id === undefined) {
-            todosService.create($scope.formData)
-                .success(function(data) {
-                    $scope.todos = data;
-                });
-        } else {
-            todosService.update($scope.formData._id, $scope.formData)
-                .success(function(data) {
-                    $scope.todos = data;
-                });
-        }
-        $scope.formData = {};
-    };
+  // UPDATE ==================================================================
+  $scope.update = function(todo) {
+    $scope.updateData(todo, todo.text);
+  };
 
-    // CREATE ==================================================================
-    $scope.edit = function (id) {
-        todosService.get(id)
-            .success(function(data) {
-                $scope.formData = data;
-                $scope.submit = 'Save';
-            });
-    };
+  // DELETE ==================================================================
+  $scope.delete = function(id) {
+    $scope.loading = true;
 
-    // UPDATE ==================================================================
-    $scope.update = function(todo) {
-        $scope.updateData(todo, todo.text);
-    };
-
-    $scope.updateData = function(todo, data) {
-        $scope.loading = true;
-
-        todosService.update(todo._id, {done: todo.done, text: data})
-            .success(function(data) {
-                $scope.todos = data;
-            });
-    };
-
-    // DELETE ==================================================================
-    $scope.delete = function(id) {
-        $scope.loading = true;
-
-        todosService.delete(id)
-            .success(function(data) {
-                $scope.loading = false;
-                $scope.todos = data;
-            });
-    };
+    todosService.delete(id)
+      .success(function(data) {
+        $scope.loading = false;
+        $scope.todos = data;
+      });
+  };
 });
